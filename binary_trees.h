93cdd799d633b9cb17380a4ab35c886542feea49
ibrahimbachiri <ibrahimbachiri205@gmal.com>
2023-11-29 06:14:36 -0800

    m

--- binary_trees.h
+++ binary_trees.h
@@ -38,4 +38,5 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 int is_complete_util(const binary_tree_t *tree, int index, int nodes_count);
 size_t binary_tree_size(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 #endif/* BINARY_TREES_H */
