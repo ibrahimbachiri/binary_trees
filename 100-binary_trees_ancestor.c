0d6a878642a755a1a6b8700ac57d8267fffb3c86
ibrahimbachiri <ibrahimbachiri205@gmal.com>
2023-11-29 02:29:07 -0800

    m

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -41,7 +41,13 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 
 	return (NULL);
 }
-/* Implement the logic to find the lowest common ancestor */
+/**
+ * binary_tree_depth - Measures the depth of a node in a binary tree.
+ *
+ * @tree: A pointer to the node to measure the depth.
+ *
+ * Return: Depth of the node in the binary tree. If tree is NULL, return 0.
+ */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
