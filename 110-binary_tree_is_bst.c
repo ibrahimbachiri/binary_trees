e07697c33f7ca01cecad9bd7ea8ada87ca1953d8
ibrahimbachiri <ibrahimbachiri205@gmal.com>
2023-11-29 06:29:25 -0800

    m

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,45 @@
+#include "binary_trees.h"
+
+/**
+ * is_bst_util - Utility function to check if a binary tree is a valid BST.
+ * @tree: A pointer to the root node of the tree to check.
+ * @min: The minimum allowed value for the nodes in the subtree.
+ * @max: The maximum allowed value for the nodes in the subtree.
+ *
+ * Return: 1 if the tree is a valid BST, 0 otherwise.
+ */
+int is_bst_util(const binary_tree_t *tree, int min, int max);
+
+/**
+ * binary_tree_is_bst - Checks if a binary tree is a valid Binary Search Tree.
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: 1 if the tree is a valid BST, 0 otherwise.
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return is_bst_util(tree, INT_MIN, INT_MAX);
+}
+
+/**
+ * is_bst_util - Utility function to check if a binary tree is a valid BST.
+ * @tree: A pointer to the root node of the tree to check.
+ * @min: The minimum allowed value for the nodes in the subtree.
+ * @max: The maximum allowed value for the nodes in the subtree.
+ *
+ * Return: 1 if the tree is a valid BST, 0 otherwise.
+ */
+int is_bst_util(const binary_tree_t *tree, int min, int max)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (tree->n <= min || tree->n >= max)
+		return (0);
+
+	return (is_bst_util(tree->left, min, tree->n) &&
+	is_bst_util(tree->right, tree->n, max));
+}
--- binary_trees.h
+++ binary_trees.h
@@ -40,4 +40,7 @@ int is_complete_util(const binary_tree_t *tree, int index, int nodes_count);
 size_t binary_tree_size(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+int is_bst_util(const binary_tree_t *tree, int min, int max);
 #endif/* BINARY_TREES_H */
