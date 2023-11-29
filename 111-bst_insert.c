4d1fc3caf05590e241442e8628f32296a335c104
ibrahimbachiri <ibrahimbachiri205@gmal.com>
2023-11-29 07:21:56 -0800

    m

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -11,33 +11,30 @@ bst_t *bst_insert(bst_t **tree, int value)
 {
 	if (tree == NULL)
 		return NULL;
-
 	if (*tree == NULL)
 	{
-	*tree = binary_tree_node(NULL, value);
-	return *tree;
+		*tree = binary_tree_node(NULL, value);
+		return *tree;
 	}
-
 	if (value < (*tree)->n)
 	{
-	if ((*tree)->left == NULL)
-	{
-	(*tree)->left = binary_tree_node(*tree, value);
-	return (*tree)->left;
-	}
-	else
-	return bst_insert(&((*tree)->left), value);
+		if ((*tree)->left == NULL)
+		{
+			(*tree)->left = binary_tree_node(*tree, value);
+			return (*tree)->left;
+		}
+		else
+			return bst_insert(&((*tree)->left), value);
 	}
 	else if (value > (*tree)->n)
 	{
-	if ((*tree)->right == NULL)
-	{
-	(*tree)->right = binary_tree_node(*tree, value);
-	return (*tree)->right;
+		if ((*tree)->right == NULL)
+		{
+			(*tree)->right = binary_tree_node(*tree, value);
+			return (*tree)->right;
+		}
+		else
+			return bst_insert(&((*tree)->right), value);
 	}
-	else
-	return bst_insert(&((*tree)->right), value);
-	}
-
 	return NULL;
 }
