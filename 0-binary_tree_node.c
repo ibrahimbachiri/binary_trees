ff18e9f08cceaa5d884f7c59289233b29e2a6dee
ibrahimbachiri <ibrahimbachiri205@gmal.com>
2023-11-29 07:03:26 -0800

    m

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,19 +1,18 @@
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Creates a binary tree node
- * @parent: Pointer to the parent node of the node to create
- * @value: Value to put in the new node
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: A pointer to the parent node.
+ * @value: The value to put in the new node.
  *
- * Return: Pointer to the new node, or NULL on failure
+ * Return: A pointer to the new node, or NULL on failure.
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
-		return (NULL);
+	return (NULL);
 
 	new_node->n = value;
 	new_node->parent = parent;
--- binary_trees.h
+++ binary_trees.h
@@ -43,4 +43,5 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 int is_bst_util(const binary_tree_t *tree, int min, int max);
+bst_t *bst_insert(bst_t **tree, int value);
 #endif/* BINARY_TREES_H */
