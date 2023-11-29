057e155e6ea234393715f0bc394074dc6ef510f2
ibrahimbachiri <ibrahimbachiri205@gmal.com>
2023-11-29 07:57:06 -0800

    m

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -15,13 +15,13 @@ bst_t *bst_search(const bst_t *tree, int value)
 
 	while (tree != NULL)
 	{
-	if (value == tree->n)
+		if (value == tree->n)
 		return (bst_t *)tree;
 
 	if (value < tree->n)
-	tree = tree->left;
+		tree = tree->left;
 	else
-	tree = tree->right;
+		tree = tree->right;
 	}
 
 	return NULL;
