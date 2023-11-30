6b8f6ed7b23fadb62717cc16dbaa47dee37974fd
ibrahimbachiri <ibrahimbachiri205@gmal.com>
2023-11-29 23:48:17 -0800

    m

--- binary_trees.h
+++ binary_trees.h
@@ -58,5 +58,5 @@ int is_bst_util(const binary_tree_t *tree, int min, int max);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
-
+int binary_tree_is_avl(const binary_tree_t *tree);
 #endif /* BINARY_TREES_H */
