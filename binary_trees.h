520fb9f827c9d13dc86d697dbf57f73c59947963
ibrahimbachiri <ibrahimbachiri205@gmal.com>
2023-11-30 06:59:16 -0800

    m

--- binary_trees.h
+++ binary_trees.h
@@ -54,7 +54,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
-int binary_tree_is_perfect(const binary_tree_t *tree);
+/*int binary_tree_is_perfect(const binary_tree_t *tree);*/
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
@@ -78,4 +78,6 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+heap_t *binary_tree_get_insertion_node(heap_t *root);
+void swap_values(int *a, int *b);
 #endif /* BINARY_TREES_H */
