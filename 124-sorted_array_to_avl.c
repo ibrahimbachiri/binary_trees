b1f71538ba9a270e91b6b87ca91704ee596658d3
ibrahimbachiri <ibrahimbachiri205@gmal.com>
2023-11-30 01:44:04 -0800

    m

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -12,17 +12,13 @@
 avl_t *sorted_array_to_avl_helper(int *array, size_t start, size_t end, avl_t *parent)
 {
 	if (start > end)
-		return NULL;
-
+	return NULL;
 	size_t mid = (start + end) / 2;
-
 	avl_t *new_node = binary_tree_node(parent, array[mid]);
 	if (new_node == NULL)
 		return NULL;
-
 	new_node->left = sorted_array_to_avl_helper(array, start, mid - 1, new_node);
 	new_node->right = sorted_array_to_avl_helper(array, mid + 1, end, new_node);
-
 	return new_node;
 }
 
@@ -37,6 +33,5 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 {
 	if (array == NULL || size == 0)
 		return NULL;
-
 	return sorted_array_to_avl_helper(array, 0, size - 1, NULL);
 }
